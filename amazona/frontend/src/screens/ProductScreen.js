d6809ed693c3a73fb9be09b4d02bb58f5b381d37
W@ngD@nY@ng <59977368+wangdanyang711@users.noreply.github.com>
2021-01-12 16:16:25 +0800

    index

--- amazona/frontend/src/screens/ProductScreen.js
+++ amazona/frontend/src/screens/ProductScreen.js
@@ -1,19 +1,30 @@
 import React from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
-import {Container, Row, Col} from 'react-bootstrap'
+import data from '../data'
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+    const product = data.products.find((x) => x._id === props.match.params.id);
+    if(!product){
+        return <div> Product Not Found...</div>
+    }
     return (
         <Container>
             <Row>
-                <Col xs={6} md={4}>
-                    xs=6 md=4
+                <Col sm={6} md={4} className="bg-warning">
+                    <img className="large" src={product.image} alt={product.name} />
                 </Col>
-                <Col xs={6} md={4}>
-                    xs=6 md=4
+                <Col sm={6} md={4} className="bg-primary">
+                    <ul>
+                        <li>
+                            <h1>{product.name}</h1>
+                        </li>
+                        <li>
+                            
+                        </li>
+                    </ul>
                 </Col>
-                <Col xs={6} md={4}>
-                    xs=6 md=4
+                <Col sm={6} md={4} className="bg-danger">
+                    sm=6 md=4
                 </Col>
             </Row>
         </Container>
